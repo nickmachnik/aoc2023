eae81cdebda2c49c6a4dd223c6c60a3e0066a9a2
nickmachnik <nick.machnik@gmail.com>
2023-12-01 12:01:35 +0100

    solve day1 part 2

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -8,18 +8,17 @@ const DIGITS: [&str; 20] = [
     "five", "six", "seven", "eight", "nine",
 ];
 
+const DIGITS_CHAR: [char; 20] = [
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '1', '2', '3', '4', '5', '6', '7', '8',
+    '9',
+];
+
 pub fn solve() -> SolutionPair {
     // Your solution here...
     let sol1: u64 = solve1("./input/day1input1.txt");
-    let sol2: u64 = solve2("./input/day1example1.txt");
+    let sol2: u64 = solve2("./input/day1input1.txt");
 
     (Solution::from(sol1), Solution::from(sol2))
-}
-
-fn solve2(filename: &str) -> u64 {
-    // let lines = read_lines(filename);
-    let mut res = 0;
-    res
 }
 
 fn solve1(filename: &str) -> u64 {
@@ -36,10 +35,57 @@ fn solve1(filename: &str) -> u64 {
     res
 }
 
+fn solve2(filename: &str) -> u64 {
+    let mut res = 0;
+    for line in load_lines(filename) {
+        let mut line_digits = vec![];
+        let last_ix = line.len() - 1;
+        let mut window_start = 0;
+        while window_start <= last_ix {
+            let mut found_digit = false;
+            for (digit_ix, digit) in DIGITS.iter().enumerate() {
+                let window_end = window_start + digit.len();
+                if window_end > last_ix + 1 {
+                    continue;
+                }
+                if **digit == line[window_start..window_end] {
+                    found_digit = true;
+                    line_digits.push(DIGITS_CHAR[digit_ix]);
+                    window_start += 1;
+                    break;
+                }
+            }
+            if !found_digit {
+                window_start += 1;
+            }
+        }
+        println!("{:?}", line_digits);
+        let calibration_code: u64 = vec![line_digits.first().unwrap(), line_digits.last().unwrap()]
+            .into_iter()
+            .collect::<String>()
+            .parse()
+            .unwrap();
+        res += calibration_code;
+    }
+    res
+}
+
 fn filter_lines(filename: &str) -> Vec<Vec<char>> {
     read_to_string(filename)
         .unwrap()
         .lines()
-        .map(|l| l.chars().filter(|c| c.is_digit(10)).collect::<Vec<char>>())
+        .map(|l| {
+            l.chars()
+                .filter(|c| c.is_ascii_digit())
+                .collect::<Vec<char>>()
+        })
+        .collect()
+}
+
+fn load_lines(filename: &str) -> Vec<String> {
+    read_to_string(filename)
+        .unwrap()
+        .lines()
+        .map(String::from)
         .collect()
 }
