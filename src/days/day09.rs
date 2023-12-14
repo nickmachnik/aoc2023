c5625062d1a2aca7a6d7106dfb4f86f2aea079a5
nickmachnik <nick.machnik@gmail.com>
2023-12-14 09:09:57 +0100

    solve days 9 and 12

--- src/days/day09.rs
+++ src/days/day09.rs
@@ -5,8 +5,83 @@ use std::fs::read_to_string;
 
 pub fn solve() -> SolutionPair {
     // Your solution here...
-    let sol1: u64 = 0;
-    let sol2: u64 = 0;
+    let sol1: i64 = solve1("./input/day9input1.txt");
+    let sol2: i64 = solve2("./input/day9input1.txt");
 
     (Solution::from(sol1), Solution::from(sol2))
+}
+
+fn solve1(filename: &str) -> i64 {
+    load_histories(filename)
+        .iter()
+        .map(|h| h.next_value())
+        .sum()
+}
+
+fn solve2(filename: &str) -> i64 {
+    // for h in load_histories(filename) {
+    //     println!("h: {:?}; pv: {:?}", h, h.prev_value());
+    // }
+    // 0
+    load_histories(filename)
+        .iter()
+        .map(|h| h.prev_value())
+        .sum()
+}
+
+#[derive(Debug)]
+struct History {
+    vals: Vec<i64>,
+}
+
+impl History {
+    fn from_line(line: &str) -> Self {
+        Self {
+            vals: line
+                .split_ascii_whitespace()
+                .map(|e| e.parse().unwrap())
+                .collect(),
+        }
+    }
+
+    fn next_value(&self) -> i64 {
+        let mut v = self.vals.clone();
+        let mut final_vals: Vec<i64> = vec![*v.last().unwrap()];
+        while !uniform(&v) {
+            v = diff(v);
+            final_vals.push(*v.last().unwrap());
+        }
+        final_vals.iter().sum()
+    }
+
+    fn prev_value(&self) -> i64 {
+        let mut v = self.vals.clone();
+        let mut first_vals: Vec<i64> = vec![*v.first().unwrap()];
+        while !uniform(&v) {
+            v = diff(v);
+            first_vals.push(*v.first().unwrap());
+        }
+        let mut res = 0;
+        first_vals.reverse();
+        for fv in first_vals {
+            res = fv - res;
+        }
+        res
+    }
+}
+
+fn uniform(v: &[i64]) -> bool {
+    v.iter().all(|e| *e == v[0])
+}
+
+fn diff(v: Vec<i64>) -> Vec<i64> {
+    v.windows(2).map(|w| w[1] - w[0]).collect()
+}
+
+fn load_histories(filename: &str) -> Vec<History> {
+    read_to_string(filename)
+        .unwrap()
+        .lines()
+        .map(History::from_line)
+        .collect()
 }
--- src/days/day12.rs
+++ src/days/day12.rs
@@ -1,56 +1,12 @@
 use crate::{Solution, SolutionPair};
-use core::panic;
-use std::{
-    collections::{HashMap, HashSet},
-    fs::read_to_string,
-};
+use std::{collections::HashMap, fs::read_to_string};
 
 ///////////////////////////////////////////////////////////////////////////////
 
-// DP = {}
-// def f(dots, blocks, i, bi, current):
-//   key = (i, bi, current)
-//   if key in DP:
-//     return DP[key]
-//   if i==len(dots):
-//     if bi==len(blocks) and current==0:
-//       return 1
-//     elif bi==len(blocks)-1 and blocks[bi]==current:
-//       return 1
-//     else:
-//       return 0
-//   ans = 0
-//   for c in ['.', '#']:
-//     if dots[i]==c or dots[i]=='?':
-//       if c=='.' and current==0:
-//         ans += f(dots, blocks, i+1, bi, 0)
-//       elif c=='.' and current>0 and bi<len(blocks) and blocks[bi]==current:
-//         ans += f(dots, blocks, i+1, bi+1, 0)
-//       elif c=='#':
-//         ans += f(dots, blocks, i+1, bi, current+1)
-//   DP[key] = ans
-//   return ans
-
-// for part2 in [False,True]:
-//   ans = 0
-//   for line in L:
-//     dots,blocks = line.split()
-//     if part2:
-//       dots = '?'.join([dots, dots, dots, dots, dots])
-//       blocks = ','.join([blocks, blocks, blocks, blocks, blocks])
-//     blocks = [int(x) for x in blocks.split(',')]
-//     DP.clear()
-//     score = f(dots, blocks, 0, 0, 0)
-//     #print(dots, blocks, score, len(DP))
-//     ans += score
-//   print(ans)
-
 pub fn solve() -> SolutionPair {
     // Your solution here...
-    // let sol1: u64 = solve1("./input/day12example1.txt");
     let sol1: u64 = solve1("./input/day12input.txt");
-    // let sol2: u64 = solve2("./input/day12input.txt");
-    let sol2: u64 = solve2("./input/day12example1.txt");
+    let sol2: u64 = solve2("./input/day12input.txt");
 
     (Solution::from(sol1), Solution::from(sol2))
 }
@@ -58,7 +14,7 @@ pub fn solve() -> SolutionPair {
 fn solve1(filename: &str) -> u64 {
     let mut res = 0;
     for line in load_lines(filename) {
-        let mut spring_row = SpringRow::from_line(&line);
+        let spring_row = SpringRow::from_line(&line);
         let loc_count = spring_row.count_configs();
         res += loc_count;
     }
@@ -68,7 +24,7 @@ fn solve1(filename: &str) -> u64 {
 fn solve2(filename: &str) -> u64 {
     let mut res = 0;
     for line in load_lines(filename) {
-        let mut spring_row = SpringRow::from_line(&line);
+        let spring_row = SpringRow::from_line(&line);
         let loc_count = spring_row.count_configs_unfolded();
         // println!("{}: {}", line, loc_count);
         res += loc_count;
@@ -79,7 +35,6 @@ fn solve2(filename: &str) -> u64 {
 struct SpringRow {
     conditions: Vec<char>,
     group_sizes: Vec<usize>,
-    mem: HashMap<String, u64>,
 }
 
 impl SpringRow {
@@ -87,8 +42,7 @@ impl SpringRow {
         let conditions = l.split_ascii_whitespace().next().unwrap().chars().collect();
         let group_sizes = l
             .split_ascii_whitespace()
-            .skip(1)
-            .next()
+            .nth(1)
             .unwrap()
             .split(',')
             .map(|e| e.parse().unwrap())
@@ -96,7 +50,6 @@ impl SpringRow {
         Self {
             conditions,
             group_sizes,
-            mem: HashMap::new(),
         }
     }
 
@@ -117,170 +70,27 @@ impl SpringRow {
         res
     }
 
-    fn count_configs(&mut self) -> u64 {
-        _count_configs(
-            self.conditions.clone(),
-            self.group_sizes.clone(),
-            Vec::new(),
+    fn count_configs(&self) -> u64 {
+        count_configs(
+            &self.conditions,
+            &self.group_sizes,
             0,
-            self.conditions.clone(),
-            &mut self.mem,
+            0,
+            0,
+            &mut HashMap::new(),
         )
     }
 
-    fn count_configs_unfolded(&mut self) -> u64 {
-        _count_configs(
-            self.unfolded_conditions(),
-            self.unfolded_group_sizes(),
-            Vec::new(),
+    fn count_configs_unfolded(&self) -> u64 {
+        count_configs(
+            &self.unfolded_conditions(),
+            &self.unfolded_group_sizes(),
             0,
-            self.conditions.clone(),
-            &mut self.mem,
+            0,
+            0,
+            &mut HashMap::new(),
         )
     }
-}
-
-fn valid_alloc(alloc: Vec<(usize, usize)>, conditions: Vec<char>) -> bool {
-    let mut alloc_pos = HashSet::new();
-    for t in alloc {
-        for p in t.0..t.0 + t.1 {
-            alloc_pos.insert(p);
-        }
-    }
-    for (pos, c) in conditions.iter().enumerate() {
-        match c {
-            '#' => {
-                if !alloc_pos.contains(&pos) {
-                    // println!("missing # alloc");
-                    return false;
-                }
-            }
-            '.' => {
-                if alloc_pos.contains(&pos) {
-                    // println!("bad . alloc");
-                    return false;
-                }
-            }
-            '?' => continue,
-            _ => panic!("unexpected condition char!"),
-        }
-    }
-    true
-}
-
-fn _arg_str(conditions: &[char], groups: &Vec<usize>) -> String {
-    format!("{:?}{:?}", conditions, groups)
-}
-
-fn _count_configs(
-    conditions: Vec<char>,
-    groups: Vec<usize>,
-    alloc: Vec<(usize, usize)>,
-    position: usize,
-    full_conditions: Vec<char>,
-    mem: &mut HashMap<String, u64>,
-) -> u64 {
-    if groups.len() == 0 {
-        if valid_alloc(alloc, full_conditions) {
-            return 1;
-        } else {
-            return 0;
-        }
-    } else if conditions.len() == 0 {
-        return 0;
-    } else if conditions.len() < (groups.iter().sum::<usize>() + groups.iter().count() - 1) {
-        return 0;
-    }
-    let mut count: u64 = 0;
-    match conditions[0] {
-        '.' => {
-            let k = _arg_str(&conditions[1..], &groups);
-            if let Some(v) = mem.get(&k) {
-                count += v;
-            } else {
-                let v = _count_configs(
-                    conditions[1..].to_vec(),
-                    groups.clone(),
-                    alloc,
-                    position + 1,
-                    full_conditions,
-                    mem,
-                );
-                mem.insert(k, v);
-                count += v;
-            }
-        }
-        '?' => {
-            // dot
-            let k = _arg_str(&conditions[1..], &groups);
-            if let Some(v) = mem.get(&k) {
-                count += v;
-            } else {
-                let v = _count_configs(
-                    conditions[1..].to_vec(),
-                    groups.clone(),
-                    alloc.clone(),
-                    position + 1,
-                    full_conditions.clone(),
-                    mem,
-                );
-                mem.insert(k, v);
-                count += v;
-            }
-            // hash
-            let mut conditions_hash = conditions.clone();
-            conditions_hash[0] = '#';
-            let k = _arg_str(&conditions_hash, &groups);
-            if let Some(v) = mem.get(&k) {
-                count += v;
-            } else {
-                let v = _count_configs(
-                    conditions_hash.clone(),
-                    groups.clone(),
-                    alloc,
-                    position,
-                    full_conditions,
-                    mem,
-                );
-                mem.insert(k, v);
-                count += v;
-            }
-        }
-        '#' => {
-            if conditions[..groups[0]].iter().all(|e| "?#".contains(*e)) {
-                let mut alloc = alloc.clone();
-                alloc.push((position, groups[0]));
-                if groups[0] == conditions.len() {
-                    if valid_alloc(alloc, full_conditions) {
-                        return 1;
-                    } else {
-                        return 0;
-                    }
-                }
-                if conditions[groups[0]] == '#' {
-                    // now we will skip a hash, that is not allowed
-                    return 0;
-                }
-                let k = _arg_str(&conditions[(groups[0] + 1)..], &groups[1..].to_vec());
-                if let Some(v) = mem.get(&k) {
-                    count += v;
-                } else {
-                    let v = _count_configs(
-                        conditions[(groups[0] + 1)..].to_vec(),
-                        groups[1..].to_vec(),
-                        alloc,
-                        position + groups[0] + 1,
-                        full_conditions,
-                        mem,
-                    );
-                    mem.insert(k, v);
-                    count += v;
-                }
-            }
-        }
-        _ => panic!("unexpected condition char!"),
-    };
-    count
 }
 
 fn load_lines(filename: &str) -> Vec<String> {
@@ -289,4 +99,48 @@ fn load_lines(filename: &str) -> Vec<String> {
         .lines()
         .map(String::from)
         .collect()
+}
+
+fn count_configs(
+    dots: &Vec<char>,
+    blocks: &Vec<usize>,
+    pos: usize,
+    block_ix: usize,
+    current_len: usize,
+    mem: &mut HashMap<(usize, usize, usize), u64>,
+) -> u64 {
+    let k = (pos, block_ix, current_len);
+    if let Some(v) = mem.get(&k) {
+        return *v;
+    }
+    if pos == dots.len() {
+        if (block_ix == blocks.len()) & (current_len == 0) {
+            return 1;
+        } else if block_ix == blocks.len() - 1 {
+            if current_len == blocks[block_ix] {
+                return 1;
+            } else {
+                return 0;
+            }
+        } else {
+            return 0;
+        }
+    }
+
+    let mut count = 0;
+    for c in ['.', '#'] {
+        if (dots[pos] == c) | (dots[pos] == '?') {
+            if (c == '.') & (current_len == 0) {
+                count += count_configs(dots, blocks, pos + 1, block_ix, current_len, mem);
+            } else if (c == '.') & (current_len > 0) & (block_ix < blocks.len()) {
+                if blocks[block_ix] == current_len {
+                    count += count_configs(dots, blocks, pos + 1, block_ix + 1, 0, mem);
+                }
+            } else if c == '#' {
+                count += count_configs(dots, blocks, pos + 1, block_ix, current_len + 1, mem);
+            }
+        }
+    }
+    mem.insert(k, count);
+    count
 }
